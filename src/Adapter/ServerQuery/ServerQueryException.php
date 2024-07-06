1d38ab30f1b1e1cce4691b5c56dc61f19259976b
mskarbek48 <macieqskarbek@gmail.com>
2024-07-06 19:17:22 +0200

    fixed event processing

---
+++ src/Adapter/ServerQuery/ServerQueryException.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	 * This file is a part of mskarbek48${PROJECT_NAME}
+	 *
+	 * @author Maciej Skarbek <macieqskarbek@gmail.com>
+	 * @copyright (c) 2024 Maciej Skarbek
+	 * @license https://opensource.org/licenses/gpl-license.php GNU Public License
+	 *
+	 * @created at 06.07.2024 16:47
+	 *
+	 * @license https://opensource.org/licenses/gpl-license.php GNU Public License
+	 **/
+
+	namespace mskarbek48\TeamspeakFramework\Adapter\ServerQuery;
+
+	class Exception extends \Exception
+	{
+
+	}
